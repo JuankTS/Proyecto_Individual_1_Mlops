417af515ca715e707f6c81e88d09bc3e3fe15e2d
JUAN <torressalasjc@gmail.com>
2024-05-25 19:23:01 -0500

    Actualizacion y documentacion

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# Proyecto_Individual_1_Mlops
+## Proyecto Integrador #1 MLOPS
+
+![alt text](image.png)
+### Contenido
+- [Planteamiento del problema](#planteamiento-del-problema)
+- [DataSets](#datasets)
+- [Explicación del repositorio](#explicación-del-repositorio)
+- [Recursos](#recursos)
+
+### Planteamiento del problema
+En este proyecto se nos propone emular el ciclo de vida de un proyecto de ML, por lo que se tocan las diferentes etapas de este ciclo, desde el proceso de ETL de los datos, el análisis y preparación de los datos hasta el montaje del modelo final. Además, se nos propone disponibilizar los datos usando el framework FastAPI. Las consultas que se deben implementar son las siguientes:
+
+- **def developer( desarrollador: str ):** Cantidad de items y porcentaje de contenido Free por año según empresa desarrolladora.
+- **def userdata( User_id: str ):** Debe devolver la cantidad de dinero gastado por el usuario, el porcentaje de recomendación en base a reviews.recommend y cantidad de items.
+- **def UserForGenre( genero: str ):** Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año de lanzamiento.
+- **def best_developer_year( año: int ):** Devuelve el top 3 de desarrolladores con juegos más recomendados por usuarios para el año dado (reviews.recommend = True y comentarios positivos).
+- **def developer_reviews_analysis( desarrolladora: str ):** Según el desarrollador, se devuelve un diccionario con el nombre del desarrollador como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor positivo o negativo.
+
+Una vez desarrollados los endpoints anteriores, también se nos propone entrenar nuestro modelo de machine learning para armar un sistema de recomendación. Para ello, se ofrecen dos propuestas de trabajo: 
+
+1. **Sistema de recomendación ítem-ítem:** Toma un ítem y, en base a su similitud con otros ítems, recomienda ítems similares. Aquí el input es un juego y el output es una lista de juegos recomendados.
+2. **Sistema de recomendación usuario-ítem:** Toma un usuario, encuentra usuarios similares y recomienda ítems que a esos usuarios les gustaron. En este caso, el input es un usuario y el output es una lista de juegos recomendados para ese usuario.
+
+Para el desarrollo del sistema de recomendación se ha usado la Similitud del coseno. 
+
+- **Sistema de recomendación ítem-ítem:**
+  - **def recomendacion_juego( id de producto ):** Ingresando el id de producto, se recibe una lista con 5 juegos recomendados similares al ingresado.
+- **Sistema de recomendación usuario-ítem:**
+  - **def recomendacion_usuario( id de usuario ):** Ingresando el id de un usuario, se recibe una lista con 5 juegos recomendados para dicho usuario.
+
+### DataSets
+Cabe aclarar que los datasets que se explican son el resultado de los procesos de ETL realizados a los datasets originales (los cuales no se encuentran en el repositorio por fines prácticos, pero su proceso de ETL está explicado en la carpeta denominada 'ETL').
+
+- **Items.csv:** Contiene las siguientes columnas:
+  - item_id: ID del juego
+  - item_name: Nombre del juego
+  - playtime_forever: Tiempo total jugado por el usuario
+  - users_id: ID del usuario
+  - items_count: Cantidad de ítems (juegos) que tiene el usuario
+
+- **Reviews.csv:** Contiene las siguientes columnas:
+  - item_id: ID del juego
+  - senti_negativo: Cantidad de reviews negativas del juego
+  - senti_neutral: Cantidad de reviews neutrales del juego
+  - senti_positivo: Cantidad de reviews positivas del juego
+
+- **steam_games.csv:** Contiene las siguientes columnas:
+  - app_name: Nombre del juego
+  - price: Precio del juego
+  - id: ID del juego
+  - developer: Desarrolladora del juego
+  - principal_genre: Género principal del juego
+  - principal_tag: Etiqueta principal del juego
+  - principal_spec: Especificación principal del juego
+  - release_year: Año de lanzamiento del juego
+
+- **user_reviews.csv:** Contiene las siguientes columnas:
+  - user_id: ID del usuario
+  - recommend_yes: Cuántas veces ha sido recomendado
+  - recommend_not: Cuántas veces ha sido no recomendado
+
+### Explicación del repositorio
+En este apartado se explica cómo está estructurado este repositorio:
+
+- **Carpeta Data:** Contiene los archivos con los datos que se usan para el desarrollo del proyecto.
+- **Carpeta ETL:** Aquí se encuentran los notebooks donde se realizó el proceso de ETL a cada uno de los archivos donde se encontraban los datos inicialmente. En cada uno de los notebooks se explica paso a paso el proceso realizado a cada base de datos.
+- **fasApi-env:** Es el entorno virtual donde se hicieron las pruebas locales de la API.
+- **EDA.ipynb:** Este notebook contiene un breve análisis de los datos resultantes de los procesos de ETL.
+- **EndPoint.ipynb:** En este notebook están los endpoints explicados y cómo es su funcionalidad.
+- **Main.py:** Es el archivo donde se encuentra el cuerpo de la API, con los endpoints desarrollados.
+- **Requirements.txt:** Librerías requeridas para hacer el deploy.
+- **Links:** Link de la API una vez hecho el deploy y el video explicativo.
+
+### Recursos
+- [Cómo funciona la búsqueda de similitud](https://pro.arcgis.com/es/pro-app/latest/tool-reference/spatial-statistics/how-similarity-search-works.htm#:~:text=El%20%C3%ADndice%20de%20similitud%20de%20coseno%20oscila%20entre%201%2C0,una%20mayor%20o%20menor%20escala.)
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+# Librerias 
 from fastapi import FastAPI, Query
 import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import os
 
-
+# Datos
 items = pd.read_csv('./Data/items.csv')
 reviews = pd.read_csv('./Data/Reviews.csv')
 games = pd.read_csv('./Data/steam_games.csv')
 users = pd.read_csv('./Data/Users.csv')
 
-
+# Inicio de la app
 app= FastAPI(title='Proyecto Integrador #1 Presentado por Juan Camilo Torres')
 
 @app.get('/',tags=['Home'])
@@ -24,73 +23,101 @@ async def about():
     return 'PROYECTO INDIVIDUAL #1 Machine Learning Operations (MLOps) presentado por Juan Camilo Torres Salas'
 
 @app.get('/Developer', tags=['GET'])
-def developer(desarrollador : str= Query(default='xropi,stev3ns')):
+def developer(desarrollador : str = Query(default='xropi,stev3ns')):
+    
+    ''' Ingresa una desarrolladora y te retorna la cantidad de juegos por año y el porcentaje 
+    
+    de esos juegos que son gratis'''
     filter_games=games[games['developer']== desarrollador]
     filter_games=filter_games.groupby('release_year').agg(Cantidad_de_Items=('app_name', 'size'),
                                              free_games=('price', lambda x: (x == 0.0).sum())).reset_index()
     filter_games['Contenido Free']= round((filter_games['free_games']/filter_games['Cantidad_de_Items'])*100, 3)
-    año= list(filter_games['release_year'])
-    cantidad_items= list(filter_games['Cantidad_de_Items'])
-    cont_free= list(filter_games['Contenido Free'])
     
-    return {'Año':año,'Cantidad de items':cantidad_items, 'Contenido Free':cont_free}
+    dic = {}
+    for _, row in filter_games.iterrows():
+        año = row['release_year']
+        cantidad_items = row['Cantidad_de_Items']
+        cont_free = row['Contenido Free']
+        
+        dic[int(año)] = {
+            'Cantidad de items': int(cantidad_items),
+            'Contenido Free': cont_free
+        }
+    
+    return dic
 
 @app.get('/UserData', tags=['GET'])
 def userdata( User_id : str= Query(default='kube134')):
+    
+    '''Ingresa el ID de usuario y te retorna infromacion sobre el usario'''
+    
     game=games[['id', 'price']]
     user= users[users['user_id']== User_id]
-    por_recommed= round((user['recommend_yes']/(user['recommend_yes']+user['recommend_not']))*100,2).iloc[0]
+    
+    por_recommed= round((user['recommend_yes']/(user['recommend_yes']+user['recommend_not']))*100,2).iloc[0] 
+    
     item_filter= items[items['users_id']==User_id]
     item_filter= item_filter.merge(game, left_on='item_id', right_on='id', how='inner')
     item_filter= item_filter.groupby('users_id').agg({'playtime_forever':'sum',
                                                       'price':'sum'}).reset_index()
+    
     usuario= item_filter['users_id'].iloc[0]
     tiempo= item_filter['playtime_forever'].iloc[0]
     dinero= item_filter['price'].iloc[0]
     
-    
-    return {
-        'Usuario': usuario,
-        'Dinero invertido': round(float(dinero), 2),
-        'Total horas de juego': int(tiempo),  
-        '% de recomendación': float(por_recommed)  
-    }
+    return {'Usuario': usuario, 'Dinero invertido': round(dinero,2), 'Tota horas de juego': tiempo,"% de recomendación":por_recommed}
 
 @app.get('/UserForGenre', tags=['GET'])
 def UserForGenre(genre: str=Query(default='Indie')):
+    
+    '''Ingresa un género de video juegos y te retorna el usuario con mas hora jugadas de ese género'''
+    
     item = items[['item_id', 'playtime_forever', 'users_id']]
     game = games[['id', 'principal_genre', 'release_year']]
     game = game[game['principal_genre'] == genre]
-    filter_game = game.merge(item, left_on='id', right_on='item_id', how='inner')
-    user_year = filter_game.groupby(['users_id','release_year']).agg({'playtime_forever': 'sum'}).reset_index()
-    max_user= user_year.loc[user_year['playtime_forever'].idxmax()]['users_id']
-    user_playtime_by_year = user_year[user_year['users_id'] == max_user][['release_year', 'playtime_forever']]
     
+    filter_game = game.merge(item, left_on='id', right_on='item_id', how='inner')
+    
+    user_year= filter_game.groupby(['users_id','release_year']).agg({'playtime_forever': 'sum'}).reset_index()
+    max_user= filter_game['users_id'].iloc[0]
+    
+    user_playtime_by_year = user_year[user_year['users_id'] == max_user][['release_year', 'playtime_forever']]
+    user_playtime_by_year.rename(columns={'release_year':'Año','playtime_forever':'Tiempo Jugado'}, inplace=True)
     year_playtime= user_playtime_by_year.to_dict('records')
+    
     return {"Usuario con más horas jugadas para Género " + genre: max_user, "Horas jugadas": year_playtime}
 
 @app.get('/best_developer_year', tags=['GET'])
 def best_developer_year( año : int= Query(default= 2012)):
+    
+    '''Ingresa un año y te retorna la desarolladora con más reviews positivas'''
+    
     game=games[['developer','app_name','id','principal_genre','release_year']]
-    game=game[game['release_year']== año]
     review=reviews[['item_id','senti_positivo']]
-    reviews_filter= game.merge(review, left_on='id', right_on='item_id', how= 'inner') 
+    game=game[game['release_year']== año]
+    
+    reviews_filter= game.merge(review, left_on='id', right_on='item_id', how= 'inner')
     reviews_filter= reviews_filter[['developer','app_name','senti_positivo']]
-    reviews_filter=reviews_filter.groupby(['developer','app_name']).agg({'senti_positivo':'sum'}).reset_index()
+    reviews_filter=reviews_filter.groupby(['developer']).agg({'senti_positivo':'sum'}).reset_index()
     reviews_filter= reviews_filter.sort_values(by= 'senti_positivo', ascending= False)
     
     developer= list(reviews_filter.iloc[:,0])
-    app= list(reviews_filter.iloc[:,1])
-    positive=list(reviews_filter.iloc[:,2])
+    positive=list(reviews_filter.iloc[:,1])
+   
+    return [{'Top 1': [{'Developer':developer[0]},{'Reviews positive':positive[0]}]},
+            {'Top 2': [{'Developer':developer[1]},{'Reviews positive':positive[1]}]},
+            {'Top 3': [{'Developer':developer[2]},{'Reviews positive':positive[2]}]}]
     
-    return [{'Puesto 1': [{'Developer':developer[0]},{'App':app[0]},{'Reviews positive':positive[0]}]},{'Puesto 2': [{'Developer':developer[1]},{'App':app[1]},{'Reviews positive':positive[1]}]},
-            {'Puesto 3': [{'Developer':developer[2]},{'App':app[2]},{'Reviews positive':positive[2]}]}]
-    
+
 @app.get('/Developer_Reviews_Analysis', tags=['GET'])
 def developer_reviews_analysis( desarrolladora : str=Query(default='Bethesda Game Studios') ):
+    
+    '''Ingresa una desarrolladora y te retorna la cantidad de reviews positivas y negativas que tiene'''
+    
     game= games[['id','developer']]
-    game= game[game['developer']== desarrolladora]
     review= reviews[['item_id', 'senti_negativo','senti_positivo']]
+    
+    game= game[game['developer']== desarrolladora]
     game_filter= game.merge(review, left_on='id', right_on='item_id', how='inner')
     game_filter= game_filter.groupby('developer').agg({'senti_negativo':'sum',
                                                        'senti_positivo':'sum'}).reset_index()
@@ -98,10 +125,14 @@ def developer_reviews_analysis( desarrolladora : str=Query(default='Bethesda Gam
     review_neg= game_filter['senti_negativo'].iloc[0]
     review_pos= game_filter['senti_positivo'].iloc[0]
     
-    return {developer:[{'Negativas':int(review_neg)}, {'Positivas': int(review_pos)}]}
+    return {developer:[{'Positivas': review_pos},{'Negativas':review_neg}]}
 
 @app.get('/Recomendacion Juego', tags=['Recomendacion'])
 def recomendacion_juego(id_de_producto: int= Query(default=413120)):
+    
+    '''Ingresa un ID de un juego y retorna una lista de juegos similares que te pueden interesar 
+    usan un modelo de similitud del coseno'''
+
     atributos = games[games['id'] == id_de_producto]
     
     if atributos.empty:
@@ -109,11 +140,13 @@ def recomendacion_juego(id_de_producto: int= Query(default=413120)):
     
     juegos = games[games['principal_genre'] == atributos['principal_genre'].iloc[0]]
     juegos= juegos[['app_name','principal_genre','principal_tag','principal_spec']]
-    juegos['caracteristicas'] = juegos[['principal_genre', 'principal_tag', 'principal_spec']].apply(lambda row: '-'.join(row.values.astype(str)), axis=1)
+    juegos['caracteristicas'] = juegos[['principal_genre', 'principal_tag', 'principal_spec']].apply(lambda row: '-'.join(row.values.astype(str)), axis=1) 
     juegos= juegos[['app_name','caracteristicas']].iloc[:1001,:]
+    
     vector= TfidfVectorizer()
     matriz= vector.fit_transform(juegos['caracteristicas'])
     matriz_similaridad= cosine_similarity(matriz)
+    
     indice_juego = juegos.index[juegos['app_name'] == atributos['app_name'].iloc[0]].tolist()[0]
     similares = list(enumerate(matriz_similaridad[indice_juego]))
     similaridades = sorted(similares, key=lambda x: x[1], reverse=True)[1:6]
@@ -123,15 +156,21 @@ def recomendacion_juego(id_de_producto: int= Query(default=413120)):
 
 @app.get('/Recomendacion Juego por usuario', tags=['Recomendacion'])
 def recomendacion_usuario( id_de_usuario: str=Query('doctr')):
+    
+    '''Ingresa un ID de un usuario y retorna una lista de juegos similares que le pueden interesar 
+    usan un modelo de similitud del coseno'''
+    
     items[['item_id','users_id']]
+    items[['item_id','users_id']] 
     item= items[items['users_id']==id_de_usuario]
     ids= item['item_id']
     item= item.merge(games, left_on='item_id', right_on='id', how= 'inner')
     item= item[['principal_genre','item_id']]
+    
     genre= pd.DataFrame(item['principal_genre'].value_counts())[:2]
     genre= genre.index.tolist()
     
-    game= games[games['principal_genre'].isin(genre)][['app_name','id','principal_genre','principal_tag','principal_spec']]
+    game= games[games['principal_genre'].isin(genre)][['app_name','id','principal_genre','principal_tag','principal_spec']] 
     game= game[~game['id'].isin(ids.tolist())].iloc[:1001,:]
     game['caracteristicas']= game[['principal_genre', 'principal_tag', 'principal_spec']].apply(lambda row: '-'.join(row.values.astype(str)), axis=1)
     game=game[['app_name','id','caracteristicas']]
@@ -143,4 +182,4 @@ def recomendacion_usuario( id_de_usuario: str=Query('doctr')):
     similaridades = sorted(similares, key=lambda x: x[0], reverse=True)[1:6]
     juegos_recomendados = [game.iloc[i[0]]['app_name'] for i in similaridades]
     
-    return {'Juegos que te pueden interesar '+ id_de_usuario: juegos_recomendados}
+    return {'Juegos similares que te pueden interesar a ti '+ id_de_usuario: juegos_recomendados}
